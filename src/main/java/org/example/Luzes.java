47f45607da2e0b3bdf8748225afa777085c4356b
IFSul <anajulia@gmail.com>
2024-09-05 22:05:19 -0300

    Commit Luzes

---
+++ src/main/java/org/example/Luzes.java
@@ -0,0 +1,84 @@
+package org.example;
+
+public class Luzes {
+    private String tipo;
+    private int intensidade;
+    private String cor;
+    private boolean estado;
+    private String modelo;
+
+
+
+    public Luzes(boolean estado){
+        this.estado = estado;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getIntensidade() {
+        return intensidade;
+    }
+
+    public void setIntensidade(int intensidade) {
+        this.intensidade = intensidade;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public boolean isestado() {
+        return estado;
+    }
+
+    public void setestado(boolean estado) {
+        this.estado = estado;
+    }
+
+    //Método toString
+    public String toString(){
+        String result=" ";
+
+        if(this.estado==true)	result += "\tLuzes ligadas!\n";
+        else result+= "\tLuzes apagadas!\n";
+
+        return result;
+    }
+
+    //Ligar
+    public void Ascender(){
+        setestado(true);
+    }
+
+    //Desligar
+    public void Apagar(){
+        setestado(false);
+    }
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+import java.util.Scanner;
+
+public class Main {
+
+        public static void main(String[] args){
+            Scanner input = new Scanner(System.in);
+
+            Luzes luz = new Luzes(true);
+
+            System.out.print(luz.toString());
+
+    }
+}
